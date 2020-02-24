ffb0a91d6b4df78360949f72520e3f17dfbf3399
Alex Reznichenko <alex.reznichenko789@gmail.com>
2020-02-24 14:24:44 +0200

    basic components structure

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,50 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Todos :todos="todos" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Todos from "./components/Todos";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Todos
+  },
+
+  data: function() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "Todo one",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "Todo two",
+          completed: true
+        },
+        {
+          id: 3,
+          title: "Todo three",
+          completed: false
+        }
+      ]
+    };
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
 }
 </style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Todos</h1>
+    <div v-for="todo in todos" :key="todo.id">
+      <TodoItem :todo="todo" />
+    </div>
+  </div>
+</template>
+<script>
+import TodoItem from "./TodoItem";
+export default {
+  name: "Todos",
+  props: ["todos"],
+  components: { TodoItem }
+};
+</script>
+<style scoped></style>
